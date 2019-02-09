c8bdad23db85f7dcc98d27782f2507216241fe38
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-02-09 10:52:35 -0700

    completed mini

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import routes from './routes';
 import './App.css';
+import Link from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -20,6 +22,16 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Link to='/'> 
+          <p>Route 1</p>
+        </Link>
+        <Link to='/2'>
+          <p>Route 1</p>
+        </Link>
+        <Link to='/3'>
+          <p>Route 1</p>
+        </Link>
+        {routes}
       </div>
     );
   }
